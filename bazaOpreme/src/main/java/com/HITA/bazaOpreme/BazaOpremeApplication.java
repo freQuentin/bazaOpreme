558952a7e4bb19517ceb70f542a005808f514d3a
freQuentin <luka.cavarovic@zg.com.hr>
2024-01-15 11:24:31 +0100

    Add files via upload

---
+++ bazaOpreme/src/main/java/com/HITA/bazaOpreme/BazaOpremeApplication.java
@@ -0,0 +1,13 @@
+package com.HITA.bazaOpreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazaOpremeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazaOpremeApplication.class, args);
+	}
+
+}
---
+++ bazaOpreme/src/test/java/com/HITA/bazaOpreme/BazaOpremeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HITA.bazaOpreme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaOpremeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
